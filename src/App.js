b5aeca11adc4682c3828e6173e482ee0b78d6e9e
benjamin farmer <viroben2001@gmail.com>
2022-12-21 20:28:27 -0800

    final code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ButtonBar from './componets/ButtonBar'
+import Gallery from './componets/Gallery'
+import {useState, useEffect} from 'react'
 
 function App() {
+  let [data, setData] = useState({})
+  let [objectId, setObjectId] = useState(12755)
+  const errorImg = 'https://placekitten.com/200/200'
+
+  let [objectImg, setObjectImg] = useState(errorImg)
+
+
+  useEffect(() => {
+    document.title='Welcome to ArtWorld'
+    setObjectImg(null)
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
+    .then(response => response.json())
+    .then(resdata => {
+      setData(resdata)
+      setObjectImg(resdata.primaryImage)
+    })
+  }, [objectId])
+
+  const handleIterate = (e) => {
+    setObjectId(objectId + Number(e.target.value))
+  }
+
+  const displayImage = () => {
+    if(!objectImg) {
+      return (
+        <h2>Loading...</h2>
+      )
+    }
+    return (
+      <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
+
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data.title}</h1>
+      <div style={{'width': '100%'}}>
+        {displayImage()}
+      </div>
+      <ButtonBar handleIterate={handleIterate} />
     </div>
   );
 }
-
 export default App;
